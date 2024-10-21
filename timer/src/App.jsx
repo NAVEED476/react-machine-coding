48529f3c100c3749ee84043972a755f4a2bb2d24
NaveedYW <naveed.shaik@yellowowl.app>
2024-10-21 22:21:56 +0530

    timer machine coding problem

--- timer/src/App.jsx
+++ timer/src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [timer, setTimer] = useState(0);
+  const [startTimer, setStartTimer] = useState(false);
+
+
+  useEffect(()=>{
+    let interval ;
+    if(startTimer){
+      interval = setInterval(()=>{
+        setTimer((prev)=>prev+1)
+      },1000)
+    }
+    return ()=>clearInterval(interval)
+
+  },[timer])
+
+  const startTime = () =>{
+    setStartTimer(true);
+  }
+
+  const stopTimer =() =>{
+    setTimer(false);
+  }
+  
   return (
     <>
-     <h1>Timer</h1>
+     <h1>Timer :: {timer}</h1>
+    <button onClick={()=>startTime()}>Start</button>
+    <button onClick={()=>stopTimer()}>Stop</button>
+    <button>Reverse</button>
+    <button>Reset</button>
     </>
   )
 }
